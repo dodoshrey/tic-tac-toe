1f9aa69eb4f54b44ccbb0ef7ccc1af91dc276ac5
dodoshrey <wahishrey@gmail.com>
2020-07-05 05:22:08 +0530

    Added difficult bot mode

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
         super()
         this.state = {
             player: 'X',
-            gameplay: [],
+            gameplay: [['','',''],['','',''],['','','']],
             winner: '',
             arr: [],
             gametype: 'Bot',
@@ -19,12 +19,23 @@ class App extends Component {
 
     onClick = (event) => {
         const {player, gameplay, arr, winner, gametype} = this.state;
-        const i = event.currentTarget.id;
+        let i = event.currentTarget.id;
         if(!arr.includes(i) && !winner) {
-            const newgp = gameplay.slice();
-            newgp[i] = player;
             const newarr = arr.filter(this.distinct).slice();
+            const newgp = gameplay.slice();
             newarr.push(i);
+            let w = -1;
+            if (i >= 0 && i <= 2)
+                w = 0;
+            else if (i >= 3 && i <= 5) {
+                w = 1;
+                i = i - 3;
+            }
+            else if (i >= 6 && i <= 8) {
+                w = 2;
+                i = i - 6;
+            }
+            newgp[w][i] = player;
             this.setState({gameplay: newgp, arr: newarr});
             
             if (gametype === '2P') {
@@ -32,50 +43,266 @@ class App extends Component {
                     this.setState({player: 'O'}, () => {this.chk_winner()});
                 else
                     this.setState({player: 'X'}, () => {this.chk_winner()});
-            } else {
+            } else if (gametype === 'Bot') {
                 this.setState({player: 'O'}, () => {this.chk_winner()});
                 if (winner)
                     this.setState({player:'O'})
                 else if (!winner && arr.length < 7)
                     this.setState({player: 'O'}, () => {this.bot_turn()});
+            } else if (gametype === 'BotHard') {
+                this.setState({player: 'O'}, () => {this.chk_winner()});
+                if (winner)
+                    this.setState({player:'O'})
+                else if (!winner && arr.length < 7)
+                    this.setState({player: 'O'}, () => {this.bot_hard_turn()});
             }
         }
     }
 
     OnePlayer = (event) => {
-        this.setState({player: 'X', gameplay: [], winner: '', arr: [], gametype: 'Bot'});
+        this.setState({player: 'X', gameplay: [['','',''],['','',''],['','','']], winner: '', arr: [], gametype: 'Bot'});
     }
     
     TwoPlayer = (event) => {
-        this.setState({player: 'X', gameplay: [], winner: '', arr: [], gametype: '2P'});
+        this.setState({player: 'X', gameplay: [['','',''],['','',''],['','','']], winner: '', arr: [], gametype: '2P'});
+    }
+
+    BotHard = (event) => {
+        this.setState({player: 'X', gameplay: [['','',''],['','',''],['','','']], winner: '', arr: [], gametype: 'BotHard'});
     }
 
     reset_game = (event) => {
-        this.setState({player: 'X', gameplay: [], winner: '', arr: []});
+        this.setState({player: 'X', gameplay: [['','',''],['','',''],['','','']], winner: '', arr: []});
+        document.querySelector("#player").innerHTML = 'PLAYER X';
     }
 
     bot_turn = (event) => {
         const {player, gameplay, arr, winner} = this.state;
-        const i = (Math.floor(Math.random() * 9)).toString();
+        let i = (Math.floor(Math.random() * 9)).toString();
         if (arr.includes(i)) {
             this.bot_turn();
         } else if (!arr.includes(i) && !winner) {
-            const newgp = gameplay.slice();
-            newgp[i] = player;
             const newarr = arr.filter(this.distinct).slice();
+            const newgp = gameplay.slice();
             newarr.push(i);
+            let w = -1;
+            if (i >= 0 && i <= 2)
+                w = 0;
+            else if (i >= 3 && i <= 5) {
+                w = 1;
+                i = i - 3;
+            }
+            else if (i >= 6 && i <= 8) {
+                w = 2;
+                i = i - 6;
+            }
+            newgp[w][i] = player;
             this.setState({gameplay: newgp, arr: newarr, player: 'X'}, () => {this.chk_winner()});
         }
+    }
+
+    bot_hard_turn = (event) => {
+        const {player, gameplay, arr} = this.state;
+        const newgp = gameplay.slice();
+        const newarr = arr.filter(this.distinct).slice();
+        let i = -1;
+        if (newarr.length === 1) {
+            if (newgp[1][1] === 'X')
+                i = 0;
+            else if (newgp[0][0] === 'X' || newgp[0][2] === 'X' || newgp[2][0] === 'X' || newgp[2][2] === 'X')
+                i = 4;
+            else if (newgp[0][1] === 'X')
+                i = 0;
+            else if (newgp[1][0] === 'X')
+                i = 0;
+            else if (newgp[1][2] === 'X')
+                i = 8;
+            else if (newgp[2][1] === 'X')
+                i = 8;
+        } else {
+            i = this.chkO();
+            if (i === -1)
+                i = this.chkX();
+            if (i === -1)
+                i = this.randO();
+        }
+        newarr.push(i);
+        let w = -1;
+        if (i >= 0 && i <= 2)
+            w = 0;
+        else if (i >= 3 && i <= 5) {
+            w = 1;
+            i = i - 3;
+        }
+        else if (i >= 6 && i <= 8) {
+            w = 2;
+            i = i - 6;
+        }
+        newgp[w][i] = player;
+        this.setState({gameplay: newgp, arr: newarr, player: 'X'}, () => {this.chk_winner()});
+    }
+
+    chkO = (element) => {
+        const {gameplay} = this.state;
+        const newgp = gameplay.slice();
+        let i = -1;
+        if (!newgp[0][0] && newgp[0][1] === 'O' && newgp[0][2] === 'O')
+            i = 0;
+        else if (newgp[0][0] === 'O' && !newgp[0][1] && newgp[0][2] === 'O')
+            i = 1;
+        else if (newgp[0][0] === 'O' && newgp[0][1] === 'O' && !newgp[0][2])
+            i = 2;
+        else if (!newgp[1][0] && newgp[1][1] === 'O' && newgp[1][2] === 'O')
+            i = 3;
+        else if (newgp[1][0] === 'O' && !newgp[1][1] && newgp[1][2] === 'O' )
+            i = 4;
+        else if (newgp[1][0] === 'O' && newgp[1][1] === 'O' && !newgp[1][2])
+            i = 5;
+        else if (!newgp[2][0] && newgp[2][1] === 'O' && newgp[2][2] === 'O')
+            i = 6;
+        else if (newgp[2][0] === 'O' && !newgp[2][1] && newgp[2][2] === 'O' )
+            i = 7;
+        else if (newgp[2][0] === 'O' && newgp[2][1] === 'O' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][0] && newgp[1][0] === 'O' && newgp[2][0] === 'O')
+            i = 0;
+        else if (newgp[0][0] === 'O' && !newgp[1][0] && newgp[2][0] === 'O' )
+            i = 3;
+        else if (newgp[0][0] === 'O' && newgp[1][0] === 'O' && !newgp[2][0])
+            i = 6;
+        else if (!newgp[0][1] && newgp[1][1] === 'O' && newgp[2][1] === 'O')
+            i = 1;
+        else if (newgp[0][1] === 'O' && !newgp[1][1] && newgp[2][1] === 'O' )
+            i = 4;
+        else if (newgp[0][1] === 'O' && newgp[1][1] === 'O' && !newgp[2][1])
+            i = 7;
+        else if (!newgp[0][2] && newgp[1][2] === 'O' && newgp[2][2] === 'O')
+            i = 2;
+        else if (newgp[0][2] === 'O' && !newgp[1][2] && newgp[2][2] === 'O' )
+            i = 5;
+        else if (newgp[0][2] === 'O' && newgp[1][2] === 'O' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][0] && newgp[1][1] ==='O' && newgp[2][2] === 'O' )
+            i = 0;
+        else if (newgp[0][0] === 'O' && !newgp[1][1] && newgp[2][2] === 'O' )
+            i = 4;
+        else if (newgp[0][0] === 'O' && newgp[1][1] === 'O' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][2] && newgp[1][1] ==='O' && newgp[2][0] === 'O' )
+            i = 2;
+        else if (newgp[0][2] === 'O' && !newgp[1][1] && newgp[2][0] === 'O' )
+            i = 4;
+        else if (newgp[0][2] === 'O' && newgp[1][1] === 'O' && !newgp[2][0])
+            i = 6;
+        return i;
+    }
+
+    chkX = (element) => {
+        const {gameplay} = this.state;
+        const newgp = gameplay.slice();
+        let i = -1;
+        if (!newgp[0][0] && newgp[0][1] === 'X' && newgp[0][2] === 'X')
+            i = 0;
+        else if (newgp[0][0] === 'X' && !newgp[0][1] && newgp[0][2] === 'X')
+            i = 1;
+        else if (newgp[0][0] === 'X' && newgp[0][1] === 'X' && !newgp[0][2])
+            i = 2;
+        else if (!newgp[1][0] && newgp[1][1] === 'X' && newgp[1][2] === 'X')
+            i = 3;
+        else if (newgp[1][0] === 'X' && !newgp[1][1] && newgp[1][2] === 'X' )
+            i = 4;
+        else if (newgp[1][0] === 'X' && newgp[1][1] === 'X' && !newgp[1][2])
+            i = 5;
+        else if (!newgp[2][0] && newgp[2][1] === 'X' && newgp[2][2] === 'X')
+            i = 6;
+        else if (newgp[2][0] === 'X' && !newgp[2][1] && newgp[2][2] === 'X' )
+            i = 7;
+        else if (newgp[2][0] === 'X' && newgp[2][1] === 'X' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][0] && newgp[1][0] === 'X' && newgp[2][0] === 'X')
+            i = 0;
+        else if (newgp[0][0] === 'X' && !newgp[1][0] && newgp[2][0] === 'X' )
+            i = 3;
+        else if (newgp[0][0] === 'X' && newgp[1][0] === 'X' && !newgp[2][0])
+            i = 6;
+        else if (!newgp[0][1] && newgp[1][1] === 'X' && newgp[2][1] === 'X')
+            i = 1;
+        else if (newgp[0][1] === 'X' && !newgp[1][1] && newgp[2][1] === 'X' )
+            i = 4;
+        else if (newgp[0][1] === 'X' && newgp[1][1] === 'X' && !newgp[2][1])
+            i = 7;
+        else if (!newgp[0][2] && newgp[1][2] === 'X' && newgp[2][2] === 'X')
+            i = 2;
+        else if (newgp[0][2] === 'X' && !newgp[1][2] && newgp[2][2] === 'X' )
+            i = 5;
+        else if (newgp[0][2] === 'X' && newgp[1][2] === 'X' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][0] && newgp[1][1] ==='X' && newgp[2][2] === 'X' )
+            i = 0;
+        else if (newgp[0][0] === 'X' && !newgp[1][1] && newgp[2][2] === 'X' )
+            i = 4;
+        else if (newgp[0][0] === 'X' && newgp[1][1] === 'X' && !newgp[2][2])
+            i = 8;
+        else if (!newgp[0][2] && newgp[1][1] ==='X' && newgp[2][0] === 'X' )
+            i = 2;
+        else if (newgp[0][2] === 'X' && !newgp[1][1] && newgp[2][0] === 'X' )
+            i = 4;
+        else if (newgp[0][2] === 'X' && newgp[1][1] === 'X' && !newgp[2][0])
+            i = 6;
+        return i;
+    }
+
+    randO = (element) => {
+        const {gameplay, arr} = this.state;
+        const newgp = gameplay.slice();
+        let i = -1;
+        if (arr.length === 3 && newgp[1][1] !== 'X')
+            i = 4;
+        else if (arr.length === 3 && newgp[1][1] === 'X') {
+            if (newgp[0][2] === 'O' && newgp[1][1] === 'X' && newgp[2][0] === 'X')
+                i = 0;
+            else if (newgp[0][2] === 'X' && newgp[1][1] === 'X' && newgp[2][0] === 'O')
+                i = 0;
+            else if (newgp[0][0] === 'O' && newgp[1][1] === 'X' && newgp[2][2] === 'X')
+                i = 2;
+            else if (newgp[0][0] === 'X' && newgp[1][1] === 'X' && newgp[2][2] === 'O')
+                i = 2;
+        } else if (arr.length === 3 && newgp[1][1] === 'O') {
+            if (newgp[0][0] === 'X' && newgp[1][1] === 'O' && newgp[2][1] === 'X')
+                i = 6;
+            else if (newgp[0][0] === 'X' && newgp[1][1] === 'O' && newgp[1][2] === 'X')
+                i = 2;
+            else if (newgp[0][0] === 'X' && newgp[1][1] === 'O' && newgp[2][2] === 'X')
+                i = 5;
+        } else {
+            if (!newgp[0][0])
+                i = 0;
+            else if (!newgp[0][2])
+                i = 2;
+            else if (!newgp[2][0])
+                i = 6;
+            else if (!newgp[2][2])
+                i = 8;
+            else if (!newgp[0][1])
+                i = 1;
+            else if (!newgp[1][0])
+                i = 3;
+            else if (!newgp[1][2])
+                i = 5;
+            else if (!newgp[2][1])
+                i = 7;
+        }
+        return i;
     }
 
     chk_winner = () => {
         const {gameplay, arr} = this.state;
         const winchk = gameplay.slice();
-        if ((winchk[0] === 'X' && winchk[1] === 'X' && winchk[2] === 'X') || (winchk[3] === 'X' && winchk[4] === 'X' && winchk[5] === 'X') || (winchk[6] === 'X' && winchk[7] === 'X' && winchk[8] === 'X') || (winchk[0] === 'X' && winchk[3] === 'X' && winchk[6] === 'X') || (winchk[2] === 'X' && winchk[5] === 'X' && winchk[8] === 'X') || (winchk[0] === 'X' && winchk[4] === 'X' && winchk[8] === 'X') || (winchk[2] === 'X' && winchk[4] === 'X' && winchk[6] === 'X') || (winchk[1] === 'X' && winchk[4] === 'X' && winchk[7] === 'X')) {
+        if ((winchk[0][0] === 'X' && winchk[0][1] === 'X' && winchk[0][2] === 'X') || (winchk[1][0] === 'X' && winchk[1][1] === 'X' && winchk[1][2] === 'X') || (winchk[2][0] === 'X' && winchk[2][1] === 'X' && winchk[2][2] === 'X') || (winchk[0][0] === 'X' && winchk[1][0] === 'X' && winchk[2][0] === 'X') || (winchk[0][1] === 'X' && winchk[1][1] === 'X' && winchk[2][1] === 'X') || (winchk[0][2] === 'X' && winchk[1][2] === 'X' && winchk[2][2] === 'X') || (winchk[0][0] === 'X' && winchk[1][1] === 'X' && winchk[2][2] === 'X') || (winchk[0][2] === 'X' && winchk[1][1] === 'X' && winchk[2][0] === 'X')) {
                 this.setState({winner: 'WINNER IS PLAYER X'});
                 document.querySelector("#player").innerHTML = '';
             }
-            else if ((winchk[0] === 'O' && winchk[1] === 'O' && winchk[2] === 'O') || (winchk[3] === 'O' && winchk[4] === 'O' && winchk[5] === 'O') || (winchk[6] === 'O' && winchk[7] === 'O' && winchk[8] === 'O') || (winchk[0] === 'O' && winchk[3] === 'O' && winchk[6] === 'O') || (winchk[2] === 'O' && winchk[5] === 'O' && winchk[8] === 'O') || (winchk[0] === 'O' && winchk[4] === 'O' && winchk[8] === 'O') || (winchk[2] === 'O' && winchk[4] === 'O' && winchk[6] === 'O') || (winchk[1] === 'O' && winchk[4] === 'O' && winchk[7] === 'O')) {
+            else if ((winchk[0][0] === 'O' && winchk[0][1] === 'O' && winchk[0][2] === 'O') || (winchk[1][0] === 'O' && winchk[1][1] === 'O' && winchk[1][2] === 'O') || (winchk[2][0] === 'O' && winchk[2][1] === 'O' && winchk[2][2] === 'O') || (winchk[0][0] === 'O' && winchk[1][0] === 'O' && winchk[2][0] === 'O') || (winchk[0][1] === 'O' && winchk[1][1] === 'O' && winchk[2][1] === 'O') || (winchk[0][2] === 'O' && winchk[1][2] === 'O' && winchk[2][2] === 'O') || (winchk[0][0] === 'O' && winchk[1][1] === 'O' && winchk[2][2] === 'O') || (winchk[0][2] === 'O' && winchk[1][1] === 'O' && winchk[2][0] === 'O')) {
                 this.setState({winner: 'WINNER IS PLAYER O'});
                 document.querySelector("#player").innerHTML = '';
             }
@@ -89,7 +316,7 @@ class App extends Component {
         const {player, gameplay, text, winner} = this.state;
         return (
             <div className='tc'>
-                <Display player={'PLAYER ' + player} val={gameplay} clk={this.onClick} Bot={this.OnePlayer} TwoP={this.TwoPlayer} reset={this.reset_game} disp_text={text} winner={winner}/>
+                <Display player={'PLAYER ' + player} val={gameplay} clk={this.onClick} Bot={this.OnePlayer} BotHard={this.BotHard} TwoP={this.TwoPlayer} reset={this.reset_game} disp_text={text} winner={winner}/>
             </div>
         );
     }
--- src/Display.js
+++ src/Display.js
@@ -1,30 +1,32 @@
 import React from 'react';
 
-function Display({val, clk, player, winner, reset, TwoP, Bot}) {
+function Display({val, clk, player, winner, reset, TwoP, Bot, BotHard}) {
 	return (
       <div>
 	      <div className='bg-pink pa3'>
-	    	<h1 style={{color: "darkblue", 'font-size': "4em"}}>Tic Tac Toe</h1>
-    		<button style={{margin: "5px", background: "white"}} onClick={Bot}>Play With Bot</button>
+	    	<h1 style={{color: "darkblue", fontSize: "4em"}}>Tic Tac Toe</h1>
+    		<button style={{margin: "5px", background: "white"}} onClick={Bot}>Bot Easy</button>
     		<button style={{margin: "5px", background: "white"}} onClick={TwoP}>Play With Partner</button>
+    		<br />
+    		<button style={{margin: "5px", background: "white"}} onClick={BotHard}>Bot Difficult</button>
     		<button style={{margin: "5px", background: "white"}} onClick={reset}>Reset Game</button>
-	    	<h2 style={{color: "darkblue", 'font-size': "2em"}} id='player'>{player}</h2>
+	    	<h2 style={{color: "darkblue", fontSize: "2em"}} id='player'>{player}</h2>
 	    	<div className='flex justify-center'>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='0' onClick={clk}><h1>{val[0]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='1' onClick={clk}><h1>{val[1]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='2' onClick={clk}><h1>{val[2]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='0' onClick={clk}><h1>{val[0][0]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='1' onClick={clk}><h1>{val[0][1]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='2' onClick={clk}><h1>{val[0][2]}</h1></button>
 	    	</div>
 	    	<div className='flex justify-center'>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='3' onClick={clk}><h1>{val[3]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='4' onClick={clk}><h1>{val[4]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='5' onClick={clk}><h1>{val[5]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='3' onClick={clk}><h1>{val[1][0]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='4' onClick={clk}><h1>{val[1][1]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='5' onClick={clk}><h1>{val[1][2]}</h1></button>
 	    	</div>
 	    	<div className='flex justify-center'>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='6' onClick={clk}><h1>{val[6]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='7' onClick={clk}><h1>{val[7]}</h1></button>
-	    		<button style={{background: "yellow"}} className='w4 h4' id='8' onClick={clk}><h1>{val[8]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='6' onClick={clk}><h1>{val[2][0]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='7' onClick={clk}><h1>{val[2][1]}</h1></button>
+	    		<button style={{background: "yellow"}} className='w4 h4' id='8' onClick={clk}><h1>{val[2][2]}</h1></button>
 	    	</div>
-	    	<h3 style={{color: "darkblue", 'font-size': "2em"}} id='msg'>{winner}</h3>
+	    	<h3 style={{color: "darkblue", fontSize: "2em"}} id='msg'>{winner}</h3>
 	      </div>
 	      <div>
 	      	<footer className='white bottom-0'>This game is made by SHREY WAHI</footer>
