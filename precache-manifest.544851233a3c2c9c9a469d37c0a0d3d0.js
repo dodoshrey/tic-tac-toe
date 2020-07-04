dc6488b89f808b0cb7b29a8cdd84f7e4c7693cdc
dodoshrey <wahishrey@gmail.com>
2020-07-04 05:51:01 +0530

    Updates

---
+++ precache-manifest.544851233a3c2c9c9a469d37c0a0d3d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a629b8b351246d56a7654cf6e3075772",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b9fcb53b7c0e320f370d",
+    "url": "/tic-tac-toe/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "af6d40c14a7785b8dbb7",
+    "url": "/tic-tac-toe/static/css/main.edf2f7de.chunk.css"
+  },
+  {
+    "revision": "b9fcb53b7c0e320f370d",
+    "url": "/tic-tac-toe/static/js/2.7e685893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.7e685893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6d40c14a7785b8dbb7",
+    "url": "/tic-tac-toe/static/js/main.ac7f372d.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
